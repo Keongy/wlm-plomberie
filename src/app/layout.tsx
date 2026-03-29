70741d5804818e0a88ec96a92d6d15a40081197c
Keongy <info.pichon@gmail.com>
2026-03-29 17:57:07 +0200

    feat: add Footer component to enhance site layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/layout/Footer';
 import './globals.css';
 import Header from '@/components/layout/Header';
 
@@ -6,8 +7,8 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
     <html lang="fr">
       <body>
         <Header />
-
         <main className="pt-16">{children}</main>
+        <Footer />
       </body>
     </html>
   );
